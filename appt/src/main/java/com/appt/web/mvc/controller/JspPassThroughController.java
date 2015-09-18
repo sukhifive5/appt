3a63ef0f5f67096f7a4b7ba79f7aa697f7e0cc2e
sukhifive <singhsac0@gmail.com>
2015-09-18 16:08:44 -0700

    adding multi

--- appt/src/main/java/com/appt/web/mvc/controller/JspPassThroughController.java
+++ appt/src/main/java/com/appt/web/mvc/controller/JspPassThroughController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class JspPassThroughController {
 	
-	@RequestMapping(value = "/{jspName}.jhtml")
+	@RequestMapping(value = {"/{jspName}.jhtml", "/templates/{jspName}.jhtml"})
 	public String jspPassThrough(@PathVariable("jspName") String jspName)
-	{
+	{		
 		return jspName;
 	}
 }
